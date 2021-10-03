d39c5b37f18b59afd37ebd85b6d09e4c8f5f1be5
vikysan77 <vikysan@outlook.com>
2021-10-03 12:22:29 +0300

    init: st-common-character-count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+export default function getCommonCharacterCount( s1, s2 ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let arrayS1 = new Array(...s1);
+  let arrayS2 = new Array(...s2);
+  let result = 0;
+
+  for (let i = 0; i < arrayS1.length; i++) {
+    let index = arrayS2.indexOf(arrayS1[i]);
+    if(index !== -1){
+      arrayS2.splice(index, 1);
+      result++;
+    }
+  }
+
+  return result; 
 }
