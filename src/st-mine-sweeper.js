e072d29855072318cf17e085d99c73a91582fc91
vikysan77 <vikysan@outlook.com>
2021-10-03 13:47:49 +0300

    init: st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,27 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function minesweeper (matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let checkVariants = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+      let resultString = [];
+      for (let j = 0; j < matrix[0].length; j++) {
+            let sum = 0;
+            for (let m = 0; m < checkVariants.length; m++) {
+                if (matrix[i + checkVariants[m][0]] !== undefined && matrix[i + checkVariants[m][0]][j + checkVariants[m][1]] !== undefined){
+                    if(matrix[i + checkVariants[m][0]][j + checkVariants[m][1]] === true){
+                        sum++;
+                    } 
+                }
+            }
+            resultString.push(sum);
+      }
+      result.push(resultString);
+  }
+  return result;
 }
